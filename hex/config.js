40a5d70561fad7ed0a9c9205036f3c9de5c44a0e
PokipokiBagre <amanojaku344@gmail.com>
2026-05-04 17:51:26 -0500

    Create config.js

---
+++ hex/config.js
@@ -0,0 +1,50 @@
+export const hexConfigs = {
+    hex1: {
+        id: 'hex1',
+        nombreCorto: 'HEX 1',
+        dbUrl: 'https://ophjcjwjgemgrhmovrvt.supabase.co',
+        dbAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9waGpjandqZ2VtZ3JobW92cnZ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ5ODY1OTIsImV4cCI6MjA5MDU2MjU5Mn0.UGZucFY1cCPUAM42eddJXAPW8jbceA2k3qchJRfkBRs',
+        storageUrl: 'https://ophjcjwjgemgrhmovrvt.supabase.co/storage/v1/object/public/imagenes-hex',
+        ui: {
+            titulo: 'HEX 1',
+            subtitulo: 'Primera campaña de rol',
+            lore: 'Esta es la configuración de la primera campaña',
+            linkRol: 'https://8chan.moe/hisrol/',
+            nombreRol: 'HILO ROL 1',
+            linkMeta: 'https://8chan.moe/hisrol/',
+            nombreMeta: 'META 2'
+        }
+    },
+    hex2: {
+        id: 'hex2',
+        nombreCorto: 'HEX 2',
+        dbUrl: 'https://lhhihjyqhtyahcedewgo.supabase.co',
+        dbAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxoaGloanlxaHR5YWhjZWRld2dvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ5OTU0OTYsImV4cCI6MjA5MDU3MTQ5Nn0.oaXgsvwAzlSuqKUQJFZd9p5VFKptUMdAaH5-lI4vSg0',
+        storageUrl: 'https://lhhihjyqhtyahcedewgo.supabase.co/storage/v1/object/public/imagenes-hex',
+        ui: {
+            titulo: 'HEX 2',
+            subtitulo: 'Segunda era del sistema',
+            lore: 'Lore específico para la campaña 2...',
+            linkRol: '#', 
+            nombreRol: 'HILO 2', 
+            linkMeta: '#', 
+            nombreMeta: 'META 2'
+        }
+    },
+    hex3: {
+        id: 'hex3',
+        nombreCorto: 'HEX 3',
+        dbUrl: 'https://bikaqwwgtbpwljkvlkdk.supabase.co',
+        dbAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJpa2Fxd3dndGJwd2xqa3Zsa2RrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ5OTc0MzksImV4cCI6MjA5MDU3MzQzOX0.Wyhei9bCz2P76Ezw54YOaEChiHd4O5ZvMCWY1Rocwxc',
+        storageUrl: 'https://bikaqwwgtbpwljkvlkdk.supabase.co/storage/v1/object/public/imagenes-hex',
+        ui: {
+            titulo: 'HEXCRAFT 3',
+            subtitulo: 'Tercera era del sistema',
+            lore: 'Lore específico para la campaña 3...',
+            linkRol: '#', 
+            nombreRol: 'HILO 3', 
+            linkMeta: '#', 
+            nombreMeta: 'META 3'
+        }
+    }
+};
